fd06822c5d85dce68cc0d8b822973094afc55a4f
Interparvus <michal.holan.on@gmail.com>
2022-05-01 19:52:14 +0200

    displayOptimizations

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import random
 import tkinter
-
+width = 1024
+height = 768
+canvas = tkinter.Canvas(bg="#333399", width=width, height=height)
+canvas.pack()
+data = []
+gap = 0
+canvas.create_line(0,height/2,width, height/2)
+canvas.create_line(width/2,0,width/2,height)
 def getData():
-    global data1, data2, data3
+    global data1, data2, data3, data
     data1 = random.randrange()
     data2 = random.randrange()
     data3 = random.randrange()
+    data = [data1, data2, data3]
+
+
+def barChart():
+    global gap, data, width, height
+    gap = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+canvas.mainloop()
